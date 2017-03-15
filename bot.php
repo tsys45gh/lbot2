86b33e4a425e267b504647c58e999223d5b5c4ef
tsys45gh <somklinn@gmail.com>
2017-03-15 22:40:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,6 +14,7 @@ if (!is_null($events['events'])) {
 			// Get text sent
 			$text = $event['message']['text'];
 			$text .= $event['source']['userId'];
+			$text .= 'xxx';
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
